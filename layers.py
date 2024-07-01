4c8511bf942e93e1e41d449cd8b0f839d773099d
Ethan Radford <ej.radford@outlook.com.au>
2024-07-01 12:30:43 +1000

    load tile,image layers

--- layers.py
+++ layers.py
@@ -2,14 +2,20 @@
 
 
 class TileLayer:
-    def __init__(self):
-        self.bitmap
-        self.tiles
-        self.canvas
-        self.size
-        self.offset
-        self.properties
-        
+    def __init__(self, layer_id:int, layer_size:tuple, layer_offset:tuple, layer_bitmap:list, **kwargs):
+        self.id:     int   = layer_id,
+        self.size:   tuple = layer_size
+        self.offset: tuple = layer_offset
+        self.bitmap: list  = layer_bitmap
+
+
+class ImageLayer:
+    def __init__(self, layer_id:int, layer_image_path:str, layer_offset:tuple, layer_pallerax:tuple, **kwargs):
+        self.id:          int   = layer_id
+        self.image_path:  str   = layer_image_path
+        self.offset:      tuple = layer_offset
+        self.pallerax:    tuple = layer_pallerax
+
 
 class EntityLayer:
     def __init__(self):
@@ -28,10 +34,6 @@ class CollisionLayer:
     def __init__(self):
         pass
 
-
-class BackgroundLayer:
-    def __init__(self):
-        pass
 
 
 
--- levels.py
+++ levels.py
@@ -1,10 +1,10 @@
 from pygame import Rect
 from tiles  import Tile
 from layers import TileLayer
+from layers import ImageLayer
 from layers import EntityLayer
 from layers import LightingLayer
 from layers import CollisionLayer
-from layers import BackgroundLayer
 
 
 class Level:
@@ -27,27 +27,73 @@ class Level:
         # self.load_imagelayers()
     
     def load_information(self, data:dict):
-        
+        """Load basic level information"""
+
         self.name     = ""
         self.size     = (data["width"], data["height"])
         self.tilesize = (data["tilewidth"], data["tileheight"])
 
 
     def load_tilelayers(self, data:list):
-        
-        layers: list = []
-        for layer in layers:
+        """Load a list of tilelayers in the level"""
+
+
+        tile_layers: list = []
+        for layer in data["layers"]:
             
             # Skip non-tilelayers
             if layer["type"] != "tilelayer": continue
 
-            # Operations
+            # Get tilelayer information
+            layer_id:     int   = layer["id"]
+            layer_size:   tuple = (layer["width"], layer["height"])
+            layer_offset: tuple = (layer["offsetx"]*self.SPRITEMODULE.scale_factor*-1, layer["offsety"]*self.SPRITEMODULE.scale_factor*-1) if "offsetx" in lay else (0, 0)
+            layer_bitmap: list  = Level.convert_bitmap_1d_to_2d(bitmap=layer["bitmap"], size=layersize)
+            kwargs:       dict  = {prop["name"]: prop["value"] for prop in layer["properties"]}
+
+            # Load tilelayer
+            tilelayer: TileLayer = TileLayer(
+                layer_id=layer_id, 
+                layer_size=layer_size,
+                layer_offset=layer_offset,
+                layer_bitmap=layer_bitmap,
+                **kwargs
+            )
+
+            tile_layers.append(tilelayer)
+
+        self.layers.extend(tile_layers)
             
 
+    def load_imagelayers(self, data:list):
+        """Load a list of imagelayers in the level"""
 
+        image_layers: list = []
+        for layer in data["layers"]:
+            
+            # Skip non-image layers
+            if layer["type"] != "imagelayer": continue
 
-    def load_imagelayers(self):
-        pass
+            # Get imagelayer information
+            layer_id:         int = layer["id"]
+            layer_image_path: str = layer["image"]
+            layer_offset:     tuple = (layer["offsetx"], layer["offsety"])  if "offsetx" in layer else (0,0)
+            layer_pallerax:   tuple = (layer["palleraxx"] if "palleraxx" in layer else 1, layer["palleraxy"] if "palleraxy" in layer else 1)
+            kwargs:           dict  = ()
+
+            # Load ImageLayer
+            imagelayer: ImageLayer = ImageLayer(
+                layer_id=layer_id,
+                layer_image_path=layer_image_path,
+                layer_offset=layer_offset,
+                layer_pallerax=layer_pallerax,
+                **kwargs
+            )
+
+            image_layers.append(imagelayer)
+
+        self.layers.extend(layers)
+
 
     def load_lightinglayers(self):
         pass
@@ -55,5 +101,5 @@ class Level:
     def load_entitylayers(self):
         pass
 
-    def colliderlayers(self):
+    def load_colliderlayers(self):
         pass
